9232fb2ab9ae10c9ca37f807002b37b539400d12
chapetta <cha.petta@hotmail.com>
2023-05-31 20:05:08 -0300

    atualizaçao

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <h1>Sistema Solar</h1>
+        <h1 className="Title-solarSystem">Sistema Solar</h1>
       </header>
     );
   }
